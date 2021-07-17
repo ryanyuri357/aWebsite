1bc1923146b22a3dff5f143e04cf148519412115
ryanyuri357 <ryan.yuri357@gmail.com>
2021-07-17 09:39:16 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import FeatureSection from "./FeatureSection";
+import CardSection from "./CardSection";
+import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return(
+        <div>
+            <NavBar />
+            <Jumbotron />
+            <FeatureSection />
+            <CardSection />
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
